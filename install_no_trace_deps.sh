3475563b4c8583c51261c8c5b83d20199a53abab
Theo Sun <theo.sun@outlook.com>
2022-08-04 21:10:31 +0800

    chore: update

--- install_no_trace_deps.sh
+++ install_no_trace_deps.sh
@@ -2,5 +2,5 @@
 
 if [ ! -d "node_modules/@sap/cds" ]; then
   echo "Installing no trace dependencies ...";
-  npm i --no-save express @sap/cds sqlite3;
+  npm i --no-save express @sap/cds sqlite3 axios;
 fi
